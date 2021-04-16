e65de160bfeae8b9d2651e5f24885a7a1afd5b36
Xenosians <fio.salis@gmail.com>
2021-04-16 13:02:11 +0700

    Add connection Files

---
+++ connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+// Connection to database
+const conn = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'dbrestapi'  
+});
+
+conn.connect((err)=>{
+   if(err) throw err;
+   console.log("Connected Sucsessfuly");
+});
+
+module.exports = conn;
