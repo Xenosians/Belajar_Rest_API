3e942ae375d5ce225c4c6c686be7edc0ba382c9f
Xenosians <fio.salis@gmail.com>
2021-04-16 22:31:03 +0700

    Showing Data From Database

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var Connection = require('./connection');
 
 exports.index = function(req,res){
-    response.ok('REST API Executed Properly')
+    response.ok('REST API Executed Properly',res)
+};
+
+//showing data from database
+exports.tampilsemuadatasiswa = function(req,res){
+    Connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
+        if(error){
+            Connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
 };
