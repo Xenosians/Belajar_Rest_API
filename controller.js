73c33b149e974b7bac9a0d291a1c73d83841d39c
Xenosians <fio.salis@gmail.com>
2021-04-17 12:47:32 +0700

    adding POST Function

--- controller.js
+++ controller.js
@@ -3,30 +3,47 @@
 var response = require('./res');
 var Connection = require('./connection');
 
-exports.index = function(req,res){
-    response.ok('REST API Executed Properly',res)
+exports.index = function (req, res) {
+    response.ok('REST API Executed Properly', res)
 };
 
 //showing data from database
-exports.tampilsemuadatasiswa = function(req,res){
-    Connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
-        if(error){
+exports.tampilsemuadatasiswa = function (req, res) {
+    Connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
             console.log(error);
-        }else{
-            response.ok(rows,res)
+        } else {
+            response.ok(rows, res)
         }
     });
 };
 
 // Showing all id from database
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
-    Connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id] ,
-        function(error,rows,fields){
+    Connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        });
+};
+
+//Adding data to database
+exports.tambahmahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    Connection.query("INSERT INTO mahasiswa (id_mahasiswa,nim,nama,jurusan) VALUES(?,?,?,?)",
+        [id_mahasiswa,nim, nama, jurusan],
+        function (error ,rows ,fields){
             if(error){
                 console.log(error);
             }else{
-                response.ok(rows,res)
+                response.ok("Added sucsessfuly",res)
             }
         });
 };
--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
 'use strict';
 
-module.exports = function(app){
+module.exports = function (app) {
     var jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
-    
-        app.route('/tampil')
-       .get(jsonku.tampilsemuadatasiswa); 
-    
-       app.route('/tampil/:id')
+
+    app.route('/tampil')
+        .get(jsonku.tampilsemuadatasiswa);
+
+    app.route('/tampil/:id')
         .get(jsonku.tampilberdasarkanid);
+    
+    app.route('/tambah')
+        .post(jsonku.tambahmahasiswa);
+
 }
