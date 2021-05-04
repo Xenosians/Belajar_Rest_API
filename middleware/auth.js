c336aa3e61092ce5c9e085fc27e925b4c76a48f0
Xenosians <fio.salis@gmail.com>
2021-05-05 06:51:21 +0700

    register user

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+var connection = require('../koneksi');
+var mysql = require('mysql');
+var md5 = require('MD5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+// registration controller
+exports.registrasi = function(req,res) {
+    var post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role:req.body.role,
+        tanggal_daftar: new Date()
+    }
+
+    var query = "SELECT email FROM ?? WHERE ??";
+    var table = ["user","email",post.email];
+
+    query = mysql.format(query,table);
+
+    connection.query(query,function(error,rows) {
+        if(error){
+            console.log(error);
+        }else{
+            if(rows,lenght==0){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query,table);
+                connection.query(query,post,function(error,rows) {
+                    if(error){
+                        console.log(error);
+                    }else{
+                        response.ok("User Added Sucsessfully",res);
+                    }
+                });
+            }else{
+                response.ok("Email Already Exist!");
+            }
+        }
+    })
+}
